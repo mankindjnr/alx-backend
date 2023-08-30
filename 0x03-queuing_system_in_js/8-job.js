8b29a22500b595bb6e966fa816f5b649bd8c2820
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-30 10:06:17 +0000

    kue 8

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,26 @@
+const kue = require('kue');
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData)
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (errorMessage) => {
+        console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+      })
+      .on('progress', (progress, data) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+
+    job.save();
+  });
+}
+
